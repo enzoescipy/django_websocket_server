89c59518d13d49da15cda20ae9a8356c7847f4a2
enzoescipy <enzoescipy@gmail.com>
2024-03-12 11:30:39 +0900

    deleted the html views and templets.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
 # mysite/urls.py
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path("chat/", include("chat.urls")),
     path("admin/", admin.site.urls),
 ]
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 https://channels.readthedocs.io/en/latest/tutorial/part_1.html#creating-the-chat-app
+
+docker run --rm -p 6379:6379 redis:7
